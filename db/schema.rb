71182247f2d703ad87b1c09cb0ad1374d1c37d41
Hassen <s3599135@student.rmit.edu.au>
2018-05-19 05:08:01 +0000

    added postgres to c9

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20180510002137) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer  "news_id"
     t.integer  "user_id"
